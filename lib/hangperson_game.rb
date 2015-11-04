f3c79cf2816a45a9b556b77242f1709b876ca2ff
Alejandro Mendez <amendez23@gmail.com>
2015-11-04 14:52:07 +0000

    Implements game functionality

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,7 +2,10 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
   # Get a word from remote "random word" service
 
   # def initialize()
@@ -10,6 +13,9 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @word_with_guesses = '-' * word.length
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -17,6 +23,35 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letter)
+    throw ArgumentError if letter == nil or letter.match(/[A-Za-z]/).nil?
+
+    rexp = /#{letter}/i
+    valid = (@guesses.match(rexp).nil? and @wrong_guesses.match(rexp).nil?)
+
+    if valid
+      found = !@word.match(rexp).nil?
+      if found
+        @guesses = @guesses + letter
+        @word_with_guesses = @word.gsub(/[^#{@guesses}]/, '-')
+      else
+        @wrong_guesses = @wrong_guesses + letter
+      end
+    end
+
+    return valid
+  end
+
+  def check_win_or_lose
+    if @word == @word_with_guesses
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
 end
